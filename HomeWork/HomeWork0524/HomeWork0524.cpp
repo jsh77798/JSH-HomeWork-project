26d7840916af59ca108cf75c2161b25dc9d9e774
vcd0h <vcd0h@DESKTOP-6UTPH71>
2023-05-24 17:24:57 +0900

    homework0524

---
+++ HomeWork/HomeWork0524/HomeWork0524.cpp
@@ -0,0 +1,242 @@
+﻿// HomeWork0524.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <conio.h>
+
+// 숙제 플레이어가 화면 바깥으로 나가지 못하게 해라.
+
+const int XLine = 20;
+const int YLine = 10;
+
+
+// 이녀석은 캡슐화된 클래스라기 보다는 기본자료형
+class int4
+{
+public:
+    int X = 0;
+    int Y = 0;
+    int Z = 0;
+    int W = 1;
+
+    void operator=(const int4& _Other)
+    {
+        X = _Other.X;
+        Y = _Other.Y;
+        Z = _Other.Z;
+        W = _Other.W;
+    }
+
+    void operator+=(const int4& _Other)
+    {
+        X += _Other.X;
+        Y += _Other.Y;
+        Z += _Other.Z;
+        W += _Other.W;
+    }
+
+};
+
+// 리스 이니셜라이저
+const int4 Left = { -1, 0 };
+const int4 Right = { 1, 0 };
+const int4 Up = { 0, -1 };
+const int4 Down = { 0, 1 };
+
+// Init('*')
+
+// [*][*][0] \n
+// [*][*][0] \n
+
+class ConsoleScreen
+{
+public:
+    ConsoleScreen()
+    {
+
+    }
+
+    void Init(char _BaseCh)
+    {
+        BaseCh = _BaseCh;
+        Clear();
+    }
+
+    void SetPixel(const int4& _Pos, char _Ch)
+    {
+       
+
+
+
+
+        ArrScreen[_Pos.Y][_Pos.X] = _Ch;
+    }
+
+    void Clear()
+    {
+        system("cls");
+        for (size_t y = 0; y < YLine; y++)
+        {
+            for (size_t x = 0; x < XLine; x++)
+            {
+                ArrScreen[y][x] = BaseCh;
+            }
+            ArrScreen[y][XLine] = 0;
+        }
+    }
+
+
+    void Print()
+    {
+        for (size_t y = 0; y < YLine; y++)
+        {
+            printf_s(ArrScreen[y]);
+            printf_s("\n");
+        }
+    }
+
+
+
+protected:
+
+
+private:
+    char BaseCh = ' ';
+    char ArrScreen[YLine][XLine + 1] = {};
+};
+
+
+
+
+class Player
+{
+public:
+    // 레퍼런스는 기본적으로 8바이트 참조입니다.
+    // int4를 그냥 16바이트입니다.
+    // int4&를 그냥 8바이트입니다.
+    // int4*를 그냥 8바이트입니다.
+
+    // 내가 넣으려는 인자가 합쳐서 8바이트가 넘어갈때
+    // 그걸 구조체나 클래스로 묶어서 참조형태로 넘기는게 대부분의 상황에서
+    // 무조건 이득이다.
+
+    int4 GetPos()
+    {
+        return Pos;
+
+    }
+
+    void AddPos(const int4& _Pos)
+    {
+        int4 newPos = Pos;
+        newPos += _Pos;
+        if (newPos.X >= 0)
+        if (newPos.X < XLine)
+        if (newPos.Y >= 0)
+        if (newPos.Y < YLine)
+         {
+            Pos = newPos;
+         }
+
+    }
+
+
+    void SetPos(const int4& _Pos, const int4& _Poss)
+    {
+        // 뭔가가 그냥 거저 된다.
+        // => 내가 만들었다.
+        // => 컴파일러나 기본문법으로 생략되어 있다.
+        
+            Pos = _Pos;
+            Pos2 = _Poss;
+
+            
+    }
+
+   
+
+    Player()
+        : Pos()
+    {
+        
+    }
+
+    Player(const Player&) = delete;
+
+    void Input()
+    {
+        char Select = (char)_getch();
+
+        switch (Select)
+        {
+        case 'a':
+            AddPos(Left);
+            break;
+
+        case 'd':
+            AddPos(Right);
+            break;
+
+        case 'w':
+            AddPos(Up);
+            break;
+
+        case 's':
+            AddPos(Down);
+            break;
+
+        default:
+            break;
+        }
+    }
+
+protected:
+
+private:
+
+
+    int4 Pos2;
+    int4 Pos;
+};
+
+// 기계어적으로 볼때 함수가 호출되고 함수의 메모리를 만들고
+// 메모리를 정리하는 담당자를 정해야하는 규칙이 있는데.
+// C++에서는 그걸 직접 정해줄수 있는 문법이 또 있는거야.
+// 컴파일러가 가장 최적의 함수호출규약을 알아서 정해줍니다.
+void /*__cdecl*/ Function()
+{
+
+}
+
+int main()
+{
+    ConsoleScreen Screen;
+    Screen.Init('*');
+
+    Player MainPlayer;
+    // 클래스의경우에는 
+    MainPlayer.SetPos({ 10, 5 },{ 20, 15 });
+    
+
+    while (true)
+    {
+        Screen.Clear();
+        Screen.SetPixel(MainPlayer.GetPos(), 'a');
+        Screen.Print();
+
+        MainPlayer.Input();
+    }
+
+
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
